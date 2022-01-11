7bfb2e1a942b7dea03b0a685f528bb9aa341f166
starcraft-j <jang@starcraft-n.co.jp>
2022-01-11 17:07:49 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+$_SESSION['switch'] = $_GET['switch'];
+if($_GET['pat'] == "a"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "b"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "c"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "d"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "e"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "f"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "g"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "h"){
+include_once('./index_a.php');
+}elseif($_GET['pat'] == "i"){
+include_once('./index_a.php');
+}else{
+include_once('./index_a.php');
+}
+?>
